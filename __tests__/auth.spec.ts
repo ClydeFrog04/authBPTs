8b43e87653abd47916f72707c143fde9eff44e95
ClydeFrog04 <egan.randal@gmail.com>
2020-09-13 21:08:14 -0600

    refactor(auth.spec.js): auth.spec.js => auth.spec.ts

--- __tests__/auth.spec.ts
+++ __tests__/auth.spec.ts
@@ -1,118 +1,117 @@
-const supertest = require('supertest');
-const server = require('../index');
-const db = require('../data/dbConfig');
+import supertest from "supertest";
+import {server} from "../index";
+import {dbConfig} from "../data/dbConfig";
 
-const username = 'auth user test'
+const username = "auth user test";
 
 const dummyUser = {
-  username: username,
-  password: 'password'
-}
+    username,
+    password: "password"
+};
 
-let token;
+let token: string | undefined;
 
 const login = async () => {
-  await supertest(server).post('/auth/register').send(dummyUser);
-  const response = await supertest(server).post('/auth/login').send(dummyUser);
-  token = response.body.token
-}
+    await supertest(server).post("/auth/register").send(dummyUser);
+    const response = await supertest(server).post("/auth/login").send(dummyUser);
+    token = response.body.token;
+};
 
 // * clears db and reseeds it to initial data before each individual test
 beforeEach(async () => {
-  await db.migrate.latest();
-  return db.seed.run();
+    await db.migrate.latest();
+    return db.seed.run();
 });
 // * closes any database connections after the tests in case it stays open
 afterAll(async () => {
-  await db.destroy();
+    await db.destroy();
 });
 
-describe('auth integration tests', () => {
+describe("auth integration tests", () => {
 
-  it('POST /auth/register, (creates a new user)', async () => {
-    const res = await supertest(server).post('/auth/register').send(dummyUser);
-    expect(res.statusCode).toBe(201);
-    expect(res.headers['content-type']).toBe("application/json; charset=utf-8");
-    expect(res.body.message).toBe('User successfully created')
-  })
-
-  it('POST /auth/register, (error status 400 and descriptive message if' +
-    ' missing password)', async () => {
-    const res = await supertest(server).post('/auth/register').send({
-      username: 'user1',
+    it("POST /auth/register, (creates a new user)", async () => {
+        const res = await supertest(server).post("/auth/register").send(dummyUser);
+        expect(res.status).toBe(201);
+        expect(res.headers["content-type"]).toBe("application/json; charset=utf-8");
+        expect(res.body.message).toBe("User successfully created");
     });
-    expect(res.statusCode).toBe(400);
-    expect(res.headers['content-type']).toBe("application/json; charset=utf-8");
-    expect(res.body.message).toBe('no password supplied')
-  })
 
-  it('POST /auth/register, (error status 400 and descriptive message if' +
-    ' missing username)', async () => {
-    const res = await supertest(server).post('/auth/register').send({
-      password: 'password',
+    it("POST /auth/register, (error status 400 and descriptive message if" +
+        " missing password)", async () => {
+        const res = await supertest(server).post("/auth/register").send({
+            username: "user1",
+        });
+        expect(res.status).toBe(400);
+        expect(res.headers["content-type"]).toBe("application/json; charset=utf-8");
+        expect(res.body.message).toBe("no password supplied");
     });
-    expect(res.statusCode).toBe(400);
-    expect(res.headers['content-type']).toBe("application/json; charset=utf-8");
-    expect(res.body.message).toBe('no username supplied')
-  })
 
-  it('POST /auth/register, (error status 400 and descriptive message if username already exists)', async () => {
-    const res = await supertest(server).post('/auth/register').send({
-      username: 'jess',
-      password: 'password'
+    it("POST /auth/register, (error status 400 and descriptive message if" +
+        " missing username)", async () => {
+        const res = await supertest(server).post("/auth/register").send({
+            password: "password",
+        });
+        expect(res.status).toBe(400);
+        expect(res.headers["content-type"]).toBe("application/json; charset=utf-8");
+        expect(res.body.message).toBe("no username supplied");
     });
-    expect(res.statusCode).toBe(400);
-    expect(res.headers['content-type']).toBe("application/json; charset=utf-8");
-    expect(res.body.message).toBe("username already taken")
-  })
 
-  it('POST /auth/login, (logs a user in)', async () => {
-    await supertest(server).post('/auth/register').send(dummyUser);
-    const res = await supertest(server).post('/auth/login').send(dummyUser);
-    expect(res.statusCode).toBe(200);
-    expect(res.headers['content-type']).toBe("application/json; charset=utf-8");
-    expect(res.body.message).toBe(`Welcome ${username}!`)
-  })
-
-  it('POST /auth/login, (error status 401 if invalid credentials)', async () => {
-    const res = await supertest(server).post('/auth/login').send({
-      username: 'jess',
-      password: 'wrongpassword'
+    it("POST /auth/register, (error status 400 and descriptive message if username already exists)", async () => {
+        const res = await supertest(server).post("/auth/register").send({
+            username: "jess",
+            password: "password"
+        });
+        expect(res.status).toBe(400);
+        expect(res.headers["content-type"]).toBe("application/json; charset=utf-8");
+        expect(res.body.message).toBe("username already taken");
     });
-    expect(res.statusCode).toBe(401);
-    expect(res.headers['content-type']).toBe("application/json; charset=utf-8");
-    expect(res.body.message).toBe('Invalid Credentials')
-  })
 
-  it('POST /auth/login, (error status 400 and descriptive message if' +
-    ' missing password)', async () => {
-    const res = await supertest(server).post('/auth/register').send({
-      username: 'jess',
+    it("POST /auth/login, (logs a user in)", async () => {
+        await supertest(server).post("/auth/register").send(dummyUser);
+        const res = await supertest(server).post("/auth/login").send(dummyUser);
+        expect(res.status).toBe(200);
+        expect(res.headers["content-type"]).toBe("application/json; charset=utf-8");
+        expect(res.body.message).toBe(`Welcome ${username}!`);
     });
-    expect(res.statusCode).toBe(400);
-    expect(res.headers['content-type']).toBe("application/json; charset=utf-8");
-    expect(res.body.message).toBe('no password supplied')
-  })
 
-  it('POST /auth/login, (error status 400 and descriptive message if' +
-    ' missing username)', async () => {
-    const res = await supertest(server).post('/auth/register').send({
-      password: 'password',
+    it("POST /auth/login, (error status 401 if invalid credentials)", async () => {
+        const res = await supertest(server).post("/auth/login").send({
+            username: "jess",
+            password: "wrongpassword"
+        });
+        expect(res.status).toBe(401);
+        expect(res.headers["content-type"]).toBe("application/json; charset=utf-8");
+        expect(res.body.message).toBe("Invalid Credentials");
     });
-    expect(res.statusCode).toBe(400);
-    expect(res.headers['content-type']).toBe("application/json; charset=utf-8");
-    expect(res.body.message).toBe('no username supplied')
-  })
 
-  it('GET /auth/logout, (can not log a user out if not logged in)', async () => {
-    await login();
-    const res = await supertest(server).get('/auth/logout').send({token: token});
-    expect(res.statusCode).toBe(200);
-  })
+    it("POST /auth/login, (error status 400 and descriptive message if" +
+        " missing password)", async () => {
+        const res = await supertest(server).post("/auth/register").send({
+            username: "jess",
+        });
+        expect(res.status).toBe(400);
+        expect(res.headers["content-type"]).toBe("application/json; charset=utf-8");
+        expect(res.body.message).toBe("no password supplied");
+    });
 
-  it('GET /auth/logout, (logs a user out if logged in)', async () => {
-    const res = await supertest(server).get('/auth/logout');
-    expect(res.statusCode).toBe(401);
-  })
-})
+    it("POST /auth/login, (error status 400 and descriptive message if" +
+        " missing username)", async () => {
+        const res = await supertest(server).post("/auth/register").send({
+            password: "password",
+        });
+        expect(res.status).toBe(400);
+        expect(res.headers["content-type"]).toBe("application/json; charset=utf-8");
+        expect(res.body.message).toBe("no username supplied");
+    });
 
+    it("GET /auth/logout, (can not log a user out if not logged in)", async () => {
+        await login();
+        const res = await supertest(server).get("/auth/logout").send({token});
+        expect(res.status).toBe(200);
+    });
+
+    it("GET /auth/logout, (logs a user out if logged in)", async () => {
+        const res = await supertest(server).get("/auth/logout");
+        expect(res.status).toBe(401);
+    });
+});
